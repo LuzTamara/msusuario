d8fd6d88bae755c74377ecdf776520e0b9f621c9
Elias <eliasbrizi@yahoo.es>
2021-05-15 12:09:06 -0300

    mapeos - enum - query - dependencias

--- src/main/java/com/brikton/labapps/msusuario/repositorios/ObraRepositorio.java
+++ src/main/java/com/brikton/labapps/msusuario/repositorios/ObraRepositorio.java
@@ -2,18 +2,20 @@ package com.brikton.labapps.msusuario.repositorios;
 
 import java.util.List;
 
+import com.brikton.labapps.msusuario.domain.Cliente;
+import com.brikton.labapps.msusuario.domain.Obra;
+import com.brikton.labapps.msusuario.domain.TipoObra;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
-
-import com.brikton.labapps.msusuario.domain.Cliente;
-import com.brikton.labapps.msusuario.domain.Obra;
 
 public interface ObraRepositorio extends JpaRepository<Obra, Integer>{
 	
 	List<Obra> findAllByCliente(Cliente cliente);
 
-	@Query("SELECT o from Obra o JOIN TipoObra to "
-			+ "WHERE to.tipoObra = :tipoObra")
-	List<Obra> findAllByTipoObra(@Param("tipoObra") Integer tipoObra); 
+	// @Query("SELECT o from Obra o JOIN TipoObra to "
+	// 		+ "WHERE to.tipoObra = :tipoObra")
+	@Query("SELECT o FROM Obra o WHERE o.tipoObra = :tipoObra")
+	List<Obra> findAllByTipoObra(@Param("tipoObra") TipoObra tipoObra); 
 }
