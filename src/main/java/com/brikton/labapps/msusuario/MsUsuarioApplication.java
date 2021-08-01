ff4cf0d1ae52844485a91661ea0af5bc60475704
miltonbernhardt <miltonbernhardt@gmail.com>
2021-07-31 21:07:54 -0300

    was configured eureka

--- src/main/java/com/brikton/labapps/msusuario/MsUsuarioApplication.java
+++ src/main/java/com/brikton/labapps/msusuario/MsUsuarioApplication.java
@@ -2,12 +2,14 @@ package com.brikton.labapps.msusuario;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-public class MsusuarioApplication {
+@EnableDiscoveryClient
+public class MsUsuarioApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MsusuarioApplication.class, args);
+		SpringApplication.run(MsUsuarioApplication.class, args);
 	}
 
 }
